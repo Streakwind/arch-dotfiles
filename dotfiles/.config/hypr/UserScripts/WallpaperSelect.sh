d35e0aedbaa3b5990590a4c7fcb7ee4c76f71393
Ruben Jing <jing.ruben@gmail.com>
2025-11-02 22:13:02 -0800

    update wallpaper select

--- dotfiles/.config/hypr/UserScripts/WallpaperSelect.sh
+++ dotfiles/.config/hypr/UserScripts/WallpaperSelect.sh
@@ -6,16 +6,21 @@ SYMLINK_PATH="$HOME/.config/hypr/current_wallpaper"
 
 cd "$WALLPAPER_DIR" || exit 1
 
-# === handle spaces name
+# === handle spaces in filenames
 IFS=$'\n'
 
 # === ICON-PREVIEW SELECTION WITH ROFI, SORTED BY NEWEST ===
-SELECTED_WALL=$(for a in $(ls -t *.jpg *.png *.gif *.jpeg 2>/dev/null); do echo -en "$a\0icon\x1f$a\n"; done | rofi -dmenu -p "")
+SELECTED_WALL=$(for a in $(ls -t *.jpg *.png *.gif *.jpeg 2>/dev/null); do
+    echo -en "$a\0icon\x1f$a\n"
+done | rofi -dmenu -p "")
 [ -z "$SELECTED_WALL" ] && exit 1
 SELECTED_PATH="$WALLPAPER_DIR/$SELECTED_WALL"
 
-# === SET WALLPAPER ===
-matugen image "$SELECTED_PATH"
+# === GENERATE COLORS WITH MATUGEN ===
+matugen image "$SELECTED_PATH"  # this updates colors.css
+
+# === SET WALLPAPER WITH SWWW ===
+swww img "$SELECTED_PATH" --transition-fps 60 --transition-type fade --transition-duration 0.5
 
 # === CREATE SYMLINK ===
 mkdir -p "$(dirname "$SYMLINK_PATH")"
