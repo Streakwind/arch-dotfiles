2e33686e66ba69a86ac4319c6883a6edd0059536
Ruben Jing <jing.ruben@gmail.com>
2025-11-02 22:32:14 -0800

    figured out keyring issues

--- dotfiles/.config/hypr/startup/keyring.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Start GNOME Keyring and export env variables
-eval $(/usr/bin/gnome-keyring-daemon --start --components=secrets,ssh,gpg)
-export SSH_AUTH_SOCK
-export GPG_AGENT_INFO
-export GNOME_KEYRING_CONTROL
